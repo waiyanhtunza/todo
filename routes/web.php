5183aa745965ededecef0ae95dd31912c68ad005
waiyanhtunza <wyh@za.com.mm>
2024-01-30 16:12:32 +0630

    hello

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,19 @@ Route::get('/',[HomeController::class,'index'])->name('home');
 
 Route::post('/tasks/store',[TaskController::class,'store'])->name('tasks.store');
 
-Route::delete('/tasks/{task}',[TaskController::class,'destroy'])->name('tasks.destroy')->middleware('auth');
+Route::delete('/tasks/{task}',[TaskController::class,'destroy'])->name('tasks.destroy');
 
 Route::get('/tasks/{task}',[TaskController::class,'show'])->name('tasks.show');
 
-Route::post('/tasks/{task}/edit',[TaskController::class,'edit'])->name('tasks.edit')->middleware('auth');
+Route::post('/tasks/{task}/edit',[TaskController::class,'edit'])->name('tasks.edit');
 
-Route::put('/tasks/{task}',[TaskController::class,'update'])->name('tasks.update')->middleware('auth');
+Route::put('/tasks/{task}',[TaskController::class,'update'])->name('tasks.update');
 
-Route::post('/tasks/{task}/complete',[TaskController::class,'complete'])->name('tasks.complete')->middleware('auth');
+Route::post('/tasks/{task}/complete',[TaskController::class,'complete'])->name('tasks.complete');
 
-Route::delete('/donetasks/{task}',[DonetaskController::class,'destroy'])->name('donetasks.destroy')->middleware('auth');
+Route::delete('/donetasks/{task}',[DonetaskController::class,'destroy'])->name('donetasks.destroy');
 
-Route::post('/donetasks/{task}',[DonetaskController::class,'show'])->name('donetasks.show')->middleware('auth');
+Route::post('/donetasks/{task}',[DonetaskController::class,'show'])->name('donetasks.show');
 
-Route::get('/donetasks',[DonetaskController::class,'index'])->name('donetasks.complete')->middleware('auth');
+Route::get('/donetasks',[DonetaskController::class,'index'])->name('donetasks.complete');
 
